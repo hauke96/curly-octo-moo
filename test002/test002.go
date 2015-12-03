afdedfaa08d27f3b1cfe434c12f33ff6538416f7
hauke96 <mail@hauke-stieler.de>
2015-12-03 15:03:31 +0100

    Did some optimization on the code. Also outsourced some elements.

--- test002/test002.go
+++ test002/test002.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/mattn/go-gtk/gdk"
 	"github.com/mattn/go-gtk/glib"
 	"github.com/mattn/go-gtk/gtk"
@@ -30,31 +29,41 @@ func main() {
 
 	layout := gtk.NewLayout(nil, nil)
 
+	menubar := createMenu()
+
 	button := gtk.NewButtonWithLabel("Weiter")
 
-	spielstand := spielstand{money: 10, haus1: haus{x: 2, y: 3, state: 0}}
+	haus1 := haus{x: 2, y: 3, state: 0}
 
-	label := gtk.NewLabel(strconv.Itoa(spielstand.money) + "\n" + strconv.Itoa(spielstand.haus1.state))
+	spielstand := spielstand{money: 10, haus1: haus1}
+
+	label := gtk.NewLabel("")
+	labelUpdate(label, &spielstand)
 
 	// start update routine
+	// TODO Channels benutzen
 	go update(&spielstand, label)
 
 	bild := gtk.NewImage()
 	setImage(bild, spielstand.haus1.state)
 
 	// lambda für die weiterschaltung
+	// Erhöhe den state nur wenn es kein upgrade gibt oder wenn wenn wir genug geld haben.
+	// Und nur bei letzterem kaufe das upgrade.
 	weiter := func(ctx *glib.CallbackContext) {
 		if spielstand.haus1.state < 6*5 {
-			spielstand.haus1.state += 1
-			if (spielstand.haus1.state)%10 == 0 && spielstand.money >= 10 {
-				fmt.Println(spielstand.haus1.state, " - ", (spielstand.haus1.state)%5)
-				setImage(bild, spielstand.haus1.state/5)
-				spielstand.money -= 10
-			} else if (spielstand.haus1.state)%10 == 0 && spielstand.money < 10 {
-				spielstand.haus1.state -= 1
+			if (spielstand.haus1.state+1)%5 != 0 {
+				spielstand.haus1.state += 1
+			} else {
+				if spielstand.money > 9 {
+					spielstand.haus1.state += 1
+					setImage(bild, spielstand.haus1.state/5)
+					spielstand.money -= 10
+				}
 			}
 		}
-		label.SetText(strconv.Itoa(spielstand.money) + "\n" + strconv.Itoa(spielstand.haus1.state))
+
+		labelUpdate(label, &spielstand)
 	}
 
 	// Ein bild kann ohne weiteres keine events, daher braucht man eine
@@ -71,25 +80,65 @@ func main() {
 	layout.Put(button, 50, 100)
 	layout.Put(label, 50, 60)
 	layout.Put(eventBox, 200, 100)
+	layout.Put(menubar, 0, 0)
+
 	window.Add(layout)
 	window.ShowAll()
 	gtk.Main()
 }
 
-func setImage(label *gtk.Image, state int) {
+func setImage(bild *gtk.Image, state int) {
 	dir, _ := filepath.Split(os.Args[0])
-	fmt.Println(strconv.Itoa(state))
-	imagefile := filepath.Join(dir, "state"+strconv.Itoa(state)+".png")
-	fmt.Println(imagefile)
-	label.SetFromFile(imagefile)
+	imagefile := dir + "/state" + strconv.Itoa(state) + ".png"
+	bild.SetFromFile(imagefile)
 }
 
 func update(stand *spielstand, label *gtk.Label) {
 	for true {
-		time.Sleep(3 * time.Second)
-		fmt.Print(stand.haus1.state)
+		time.Sleep(time.Second)
 		stand.money += (stand.haus1.state / 5)
-		fmt.Print(" - ", stand.haus1.state, " - ", (stand.haus1.state / 5), "\n")
-		label.SetText(strconv.Itoa(stand.money) + "\n" + strconv.Itoa(stand.haus1.state))
+		labelUpdate(label, stand)
 	}
+}
+
+func labelUpdate(label *gtk.Label, spielstand *spielstand) {
+	label.SetText(strconv.Itoa(spielstand.money) + "\n" + strconv.Itoa(spielstand.haus1.state))
+}
+
+func createMenu() *gtk.MenuBar {
+	menubar := gtk.NewMenuBar()
+	vpaned := gtk.NewVPaned()
+
+	//--------------------------------------------------------
+	// GtkMenuItem
+	//--------------------------------------------------------
+	cascademenu := gtk.NewMenuItemWithMnemonic("_File")
+	menubar.Append(cascademenu)
+	submenu := gtk.NewMenu()
+	cascademenu.SetSubmenu(submenu)
+
+	var menuitem *gtk.MenuItem
+	menuitem = gtk.NewMenuItemWithMnemonic("E_xit")
+	menuitem.Connect("activate", func() {
+		gtk.MainQuit()
+	})
+	submenu.Append(menuitem)
+
+	cascademenu = gtk.NewMenuItemWithMnemonic("_View")
+	menubar.Append(cascademenu)
+	submenu = gtk.NewMenu()
+	cascademenu.SetSubmenu(submenu)
+
+	checkmenuitem := gtk.NewCheckMenuItemWithMnemonic("_Disable")
+	checkmenuitem.Connect("activate", func() {
+		vpaned.SetSensitive(!checkmenuitem.GetActive())
+	})
+	submenu.Append(checkmenuitem)
+
+	cascademenu = gtk.NewMenuItemWithMnemonic("_Help")
+	menubar.Append(cascademenu)
+	submenu = gtk.NewMenu()
+	cascademenu.SetSubmenu(submenu)
+
+	return menubar
 }
